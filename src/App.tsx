5b616f606a864cd71caeb0bd7a76bcbe341cfa9c
Izabela S <izabelasiudak@gmail.com>
2022-03-09 20:39:28 +0000

    wouter test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route } from "wouter";
+import InboxPage from "pages/inbox-page";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Link href="/users/1">
+        <p>Profile</p>
+      </Link>
+      <Link href="/inbox">
+        <p>inbox</p>
+      </Link>
+      <Link href="/about">
+        <p>About</p>
+      </Link>
+
+      <Route path="/about">About Us</Route>
+      <Route path="/users/:name">
+        {(params) => <div>Hello, {params.name}!</div>}
+      </Route>
+      <Route path="/inbox" component={InboxPage} />
     </div>
   );
-}
+};
 
 export default App;
